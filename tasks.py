9ca522c16af24793bfb457f1828970b9a30e1e5e
Madeline Scyphers <madeline.scyphers@metergroup.com>
2022-03-31 12:30:19 -0400

    CLI(invoke and pytest): Fixes for invoke on windows, and pytest warning suppression

--- tasks.py
+++ tasks.py
@@ -2,8 +2,16 @@
 invoke --list
 from the command line for a list of all available commands.
 """
+import os
+import sys
 
 from invoke import task
+
+is_python_37 = sys.version_info[0] == 3 and sys.version_info[1] == 7
+greater_than_36 = sys.version_info > (3, 6)
+
+
+POSIX = os.name == "posix"
 
 
 @task
@@ -11,13 +19,21 @@ def black(
     command,
 ):
     """Runs black (autoformatter) on all .py files recursively"""
-    print(
-        """
+    if sys.version_info > (3, 6):
+        print(
+            """
 Running Black the Python code formatter
 =======================================
 """
-    )
-    command.run("black .", echo=True, pty=True)
+        )
+        command.run("black .", echo=True, pty=POSIX)
+    else:
+        print(
+            """
+Black formatting should be ran on Python 3.7 or higher, skipping
+================================================================
+"""
+        )
 
 
 @task
@@ -25,13 +41,21 @@ def isort(
     command,
 ):
     """Runs isort (import sorter) on all .py files recursively"""
-    print(
-        """
+    if POSIX or sys.version_info > (3,):
+        print(
+            """
 Running isort the Python code import sorter
 ===========================================
 """
-    )
-    command.run("isort .", echo=True, pty=True)
+        )
+        command.run("isort .", echo=True, pty=POSIX)
+    else:
+        print(
+            """
+On Windows, isort import sorter should be ran on Python 3, skipping
+===================================================================
+"""
+        )
 
 
 @task
@@ -45,7 +69,7 @@ Running flake8 a Python code linter
 ===================================
 """
     )
-    command.run("flake8", echo=True, pty=True)
+    command.run("flake8", echo=True, pty=POSIX)
 
 
 @task(pre=[black, isort, lint])
@@ -79,7 +103,7 @@ Running pytest the test framework
 =================================
 """
     )
-    command.run("python -m pytest .", echo=True, pty=True)
+    command.run("python -m pytest .", echo=True, pty=POSIX)
 
 
 @task
@@ -94,7 +118,7 @@ Running Sphinx to test the docs building
 ========================================
 """
     )
-    command.run("sphinx-build -b html docs docs/_build/html", echo=True, pty=True)
+    command.run("sphinx-build -b html docs docs/_build/html", echo=True, pty=POSIX)
 
 
 @task(pre=[black, isort, lint, pytest, docs])
